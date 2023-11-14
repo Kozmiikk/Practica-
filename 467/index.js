15c1ca116893f90f9ea64ea65fcab2f571c11b73
jkk <email@example.com>
2023-11-14 16:17:05 +0300

    .

--- 467/index.js
+++ 467/index.js
@@ -6,6 +6,7 @@ for (let i = 0; i < 5; i++) {
     let tr = document.createElement('tr')
     for (let j = 0; j < 5; j++) {
         let td = document.createElement('td')
+
         td.textContent = count
         count += 2
         tr.appendChild(td)
